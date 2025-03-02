f0b4bc879289b225a772d8619001255c495e0a55
iamk3nt <kentrameez@gmail.com>
2025-03-02 16:00:02 +0530

    Updated SQL

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,5 +1,5 @@
 -- Create Fact Table: EquipmentEvent
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.EquipmentEvent (
+CREATE TABLE IF NOT EXISTS EquipmentEvent (
     eventID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     eventCreatedDateTime TIMESTAMP, -- When the event was created
     eventClassifierCode VARCHAR(50), -- Classifier code for the event
@@ -10,11 +10,15 @@ CREATE TABLE IF NOT EXISTS logistic_datawarehouse.EquipmentEvent (
     transportCallID CHAR(16), -- Foreign key to TransportCall
     locationID CHAR(16), -- Foreign key to EventLocation
     sealID CHAR(16), -- Foreign key to Seals
-    documentReferenceID CHAR(16) -- Foreign key to DocumentReferences
+    documentReferenceID CHAR(16), -- Foreign key to DocumentReferences
+    CONSTRAINT FK_EquipmentEvent_TransportCall FOREIGN KEY (transportCallID) REFERENCES TransportCall(transportCallID),
+    CONSTRAINT FK_EquipmentEvent_EventLocation FOREIGN KEY (locationID) REFERENCES EventLocation(locationID),
+    CONSTRAINT FK_EquipmentEvent_Seals FOREIGN KEY (sealID) REFERENCES Seals(sealID),
+    CONSTRAINT FK_EquipmentEvent_DocumentReferences FOREIGN KEY (documentReferenceID) REFERENCES DocumentReferences(documentReferenceID)
 );
 
 -- Create Dimension Table: DocumentReferences
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.DocumentReferences (
+CREATE TABLE IF NOT EXISTS DocumentReferences (
     documentReferenceID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     documentIndex INT, -- Index number of the document
     documentReferenceType VARCHAR(100), -- Type of document reference
@@ -22,17 +26,19 @@ CREATE TABLE IF NOT EXISTS logistic_datawarehouse.DocumentReferences (
 );
 
 -- Create Dimension Table: EventLocation
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.EventLocation (
+CREATE TABLE IF NOT EXISTS EventLocation (
     locationID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     latitude DECIMAL(10, 7), -- Latitude coordinate
     longitude DECIMAL(10, 7), -- Longitude coordinate
     unlocationCode VARCHAR(20), -- UN location code
     facilityID CHAR(16), -- Foreign key to Facility
-    addressID CHAR(16) -- Foreign key to Address
+    addressID CHAR(16), -- Foreign key to Address
+    CONSTRAINT FK_EventLocation_Facility FOREIGN KEY (facilityID) REFERENCES Facility(facilityID),
+    CONSTRAINT FK_EventLocation_Address FOREIGN KEY (addressID) REFERENCES Address(addressID)
 );
 
 -- Create Dimension Table: Address
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Address (
+CREATE TABLE IF NOT EXISTS Address (
     addressID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     stateRegion VARCHAR(100), -- State or region
     streetNumber VARCHAR(20), -- Street number
@@ -45,7 +51,7 @@ CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Address (
 );
 
 -- Create Dimension Table: Facility
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Facility (
+CREATE TABLE IF NOT EXISTS Facility (
     facilityID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     facilityCodeListProvider VARCHAR(100), -- Provider of facility codes
     facilityCode VARCHAR(50), -- Facility code
@@ -53,18 +59,20 @@ CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Facility (
 );
 
 -- Create Dimension Table: TransportCall
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.TransportCall (
+CREATE TABLE IF NOT EXISTS TransportCall (
     transportCallID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     carrierServiceCode VARCHAR(50), -- Carrier service code
     exportVoyageNumber INT, -- Export voyage number
     importVoyageNumber INT, -- Import voyage number
     modeOfTransport VARCHAR(50), -- Mode of transport (Air, Sea, Road)
     vesselID CHAR(16), -- Foreign key to Vessel
-    facilityID CHAR(16) -- Foreign key to Facility
+    facilityID CHAR(16), -- Foreign key to Facility
+    CONSTRAINT FK_TransportCall_Vessel FOREIGN KEY (vesselID) REFERENCES Vessel(vesselID),
+    CONSTRAINT FK_TransportCall_Facility FOREIGN KEY (facilityID) REFERENCES Facility(facilityID)
 );
 
 -- Create Dimension Table: Vessel
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Vessel (
+CREATE TABLE IF NOT EXISTS Vessel (
     vesselID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     vesselCallSignNumber VARCHAR(50), -- Vessel call sign number
     vesselFlag VARCHAR(50), -- Country flag of the vessel
@@ -75,53 +83,10 @@ CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Vessel (
 );
 
 -- Create Dimension Table: Seals
-CREATE TABLE IF NOT EXISTS logistic_datawarehouse.Seals (
+CREATE TABLE IF NOT EXISTS Seals (
     sealID CHAR(16) PRIMARY KEY, -- SHA-256 truncated 16-character ID, primary key
     sealIndex INT, -- Index number of the seal
     sealNumber VARCHAR(100), -- Seal number
     sealSource VARCHAR(100), -- Source of the seal
     sealType VARCHAR(50) -- Type of seal
 );
-
--- Add Foreign Key Constraints to EquipmentEvent
-ALTER TABLE logistic_datawarehouse.EquipmentEvent
-ADD CONSTRAINT FK_EquipmentEvent_DocumentReferences
-FOREIGN KEY (documentReferenceID)
-REFERENCES logistic_datawarehouse.DocumentReferences (documentReferenceID);
-
-ALTER TABLE logistic_datawarehouse.EquipmentEvent
-ADD CONSTRAINT FK_EquipmentEvent_EventLocation
-FOREIGN KEY (locationID)
-REFERENCES logistic_datawarehouse.EventLocation (locationID);
-
-ALTER TABLE logistic_datawarehouse.EquipmentEvent
-ADD CONSTRAINT FK_EquipmentEvent_Seals
-FOREIGN KEY (sealID)
-REFERENCES logistic_datawarehouse.Seals (sealID);
-
-ALTER TABLE logistic_datawarehouse.EquipmentEvent
-ADD CONSTRAINT FK_EquipmentEvent_TransportCall
-FOREIGN KEY (transportCallID)
-REFERENCES logistic_datawarehouse.TransportCall (transportCallID);
-
--- Add Foreign Key Constraints to EventLocation
-ALTER TABLE logistic_datawarehouse.EventLocation
-ADD CONSTRAINT FK_EventLocation_Facility
-FOREIGN KEY (facilityID)
-REFERENCES logistic_datawarehouse.Facility (facilityID);
-
-ALTER TABLE logistic_datawarehouse.EventLocation
-ADD CONSTRAINT FK_EventLocation_Address
-FOREIGN KEY (addressID)
-REFERENCES logistic_datawarehouse.Address (addressID);
-
--- Add Foreign Key Constraints to TransportCall
-ALTER TABLE logistic_datawarehouse.TransportCall
-ADD CONSTRAINT FK_TransportCall_Vessel
-FOREIGN KEY (vesselID)
-REFERENCES logistic_datawarehouse.Vessel (vesselID);
-
-ALTER TABLE logistic_datawarehouse.TransportCall
-ADD CONSTRAINT FK_TransportCall_Facility
-FOREIGN KEY (facilityID)
-REFERENCES logistic_datawarehouse.Facility (facilityID);
